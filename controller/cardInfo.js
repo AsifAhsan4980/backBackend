c9dcadd2bc41a3d8c0d17b6ca38ea5bbf9fd7157
Asif Ahsan <56015056+AsifAhsan4980@users.noreply.github.com>
2022-07-31 02:44:39 +0600

    update

--- controller/cardInfo.js
+++ controller/cardInfo.js
@@ -2,6 +2,9 @@ import Card from "../model/card.js";
 
 
 const submitInfo = async (req, res) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     console.log(req.body)
     const {fullName, empolyer, email, phoneNumber, district, income, dob} = req.body
 
